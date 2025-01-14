153004b5b8ed00e24d9e90b556f9ee0626125016
MNaufal2506 <naufaljamil48@gmail.com>
2025-01-14 11:00:39 +0700

    Tugas 6

---
+++ M5/tugas 6.js
@@ -0,0 +1,22 @@
+// No.1
+let dressSize = 45;
+console.log(typeof dressSize)
+
+// Jawaban 1
+dressSize = String(dressSize);
+console.log(dressSize)
+
+// Jawaban 2
+dressSize = +dressSize;
+console.log(dressSize)
+
+
+// No.2
+let isLight = true;
+console.log(typeof isLight)
+
+// Jawaban 1
+isLight = String(isLight);
+console.log(typeof isLight)
+
+
